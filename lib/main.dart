ff2b32566d76664f3c5427d04fbdbd936440aa38
RyanMZ <rymontanoz@gmail.com>
2022-11-10 11:29:08 -0300

    rascunho de column e começo do Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -10,83 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text('Primeiros Passos'),
         ),
-        home: Container(
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.purple,
-                    width: 200,
-                    height: 200,
-                  ),
-                  Container(
-                    color: Colors.pinkAccent,
-                    width: 100,
-                    height: 100,
-                  ),
-                ],
-              ),
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.pinkAccent,
-                    width: 200,
-                    height: 200,
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    width: 100,
-                    height: 100,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    height: 100,
-                    width: 100,
-                  ),
-                  Container(
-                    color: Colors.orangeAccent,
-                    height: 100,
-                    width: 100,
-                  ),
-                  Container(
-                    color: Colors.yellowAccent,
-                    height: 100,
-                    width: 100,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.cyanAccent,
-                height: 50,
-                width: 350,
-                child: const Text(
-                  'Mosaico',
-                  style: TextStyle(color: Colors.black, fontSize: 40),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print('Sequência de autodestruição iniciada');
-                  },
-                  child: const Text('Recomendo não pressionar o botão'))
-            ],
-          ),
-        ));
+        body: Container(color: Colors.black),
+        floatingActionButton: FloatingActionButton(onPressed: ),
+      ),
+    );
   }
 }
---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,74 @@
+// Container(
+//           color: Colors.white,
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               Stack(
+//                 alignment: AlignmentDirectional.center,
+//                 children: [
+//                   Container(
+//                     color: Colors.purple,
+//                     width: 200,
+//                     height: 200,
+//                   ),
+//                   Container(
+//                     color: Colors.pinkAccent,
+//                     width: 100,
+//                     height: 100,
+//                   ),
+//                 ],
+//               ),
+//               Stack(
+//                 alignment: AlignmentDirectional.center,
+//                 children: [
+//                   Container(
+//                     color: Colors.pinkAccent,
+//                     width: 200,
+//                     height: 200,
+//                   ),
+//                   Container(
+//                     color: Colors.purple,
+//                     width: 100,
+//                     height: 100,
+//                   ),
+//                 ],
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 children: [
+//                   Container(
+//                     color: Colors.red,
+//                     height: 100,
+//                     width: 100,
+//                   ),
+//                   Container(
+//                     color: Colors.orangeAccent,
+//                     height: 100,
+//                     width: 100,
+//                   ),
+//                   Container(
+//                     color: Colors.yellowAccent,
+//                     height: 100,
+//                     width: 100,
+//                   ),
+//                 ],
+//               ),
+//               Container(
+//                 color: Colors.cyanAccent,
+//                 height: 50,
+//                 width: 350,
+//                 child: const Text(
+//                   'Mosaico',
+//                   style: TextStyle(color: Colors.black, fontSize: 40),
+//                   textAlign: TextAlign.center,
+//                 ),
+//               ),
+//               ElevatedButton(
+//                   onPressed: () {
+//                     print('Sequência de autodestruição iniciada');
+//                   },
+//                   child: const Text('Recomendo não pressionar o botão'))
+//             ],
+//           ),
+//         ));
