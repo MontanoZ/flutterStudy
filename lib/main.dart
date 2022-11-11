119aeff70752f0bbf4325e4ac7b0469080830328
RyanMZ <rymontanoz@gmail.com>
2022-11-11 11:13:28 -0300

    m

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
+        primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         backgroundColor: Colors.purpleAccent,
